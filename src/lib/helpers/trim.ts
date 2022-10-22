4928f2f8c4277f3196f618c8679105872cb2fca6
Github Action <github@jorgeferreiro.com>
2022-10-22 16:52:59 +0200

    feat: replace trim by the #1 most popular trim library
    While the trim from mailcheck was ok, we can do it better

--- src/lib/helpers/trim.ts
+++ src/lib/helpers/trim.ts
@@ -1,6 +1,30 @@
+const WHITESPACE_PATTERN = /\s/;
+
+/**
+ * Just in case a browser doesn't support .trim
+ * Copied from https://github.com/Trott/trim
+ * Credits to them
+ */
 function trim(str: string): string {
-  // trim() not exist in old IE!
-  return str.replace(/^\s*/, '').replace(/\s*$/, '');
+  if (str.trim) {
+    return str.trim();
+  }
+  return right(left(str));
+}
+
+function left(str: string): string {
+  if (str.trimLeft) return str.trimLeft();
+
+  return str.replace(/^\s\s*/, '');
+}
+
+function right(str: string): string {
+  if (str.trimRight) return str.trimRight();
+
+  let i = str.length;
+  while (WHITESPACE_PATTERN.test(str.charAt(--i)));
+
+  return str.slice(0, i + 1);
 }
 
 export default trim;
