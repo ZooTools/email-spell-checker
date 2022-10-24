d64ff3a2cb303770e303f5370d66ce7339a2c771
Oskar Eriksson <oskar.eriksson9@gmail.com>
2022-10-24 15:34:35 +0200

    feat: Export POPULAR_TLDS (#11)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import run from './lib/run';
-import { POPULAR_DOMAINS } from './lib/config';
+import { POPULAR_DOMAINS, POPULAR_TLDS } from './lib/config';
 
 const MailSpellChecker = {
   run,
   POPULAR_DOMAINS,
+  POPULAR_TLDS,
 };
 
-export { run, POPULAR_DOMAINS };
+export { run, POPULAR_DOMAINS, POPULAR_TLDS };
 export default MailSpellChecker;
