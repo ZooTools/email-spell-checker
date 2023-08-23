73507f79a346d84e6722d053ba039a79ab8b3b03
Eray Alkış <lolpo12345@outlook.com>
2023-08-24 00:33:57 +0300

    docs: adds documentation to JavaScript files/sections of components
    Adds documentation to ./demos/html/index.js, ./demos/react/src/components/AppForm.jsx,
    ./demos/vue/src/components/AppForm.vue.

--- demos/html/index.js
+++ demos/html/index.js
@@ -7,35 +7,41 @@ const warningText = document.getElementById('warning-text');
 const warningEmail = document.getElementById('warning-email');
 const warningDismiss = document.getElementById('warning-dismiss');
 
+// Timer for debouncing suggestedEmail updates
 let debounceTimer;
 
+// Takes a callback and a time argument, clears `debounceTimer` if exists, and returns the new timer
 const debounce = (callback, time) => {
-  window.clearTimeout(debounceTimer);
+  if (debounceTimer) window.clearTimeout(debounceTimer);
   debounceTimer = window.setTimeout(callback, time);
 };
 
 emailInput.addEventListener('keyup', () => {
   debounce(() => {
     const email = emailInput.value;
-
+    // Get suggested email
     const suggestedEmail = emailSpellChecker.run({
       email,
     });
 
+    // If return value exists
     if (suggestedEmail) {
+      // Make the warning container visible,
       warningContainer.classList.add('view');
 
+      // and add warning text
       warningText.innerText = `Did you mean`;
       warningEmail.innerText = `${suggestedEmail.full}?`;
     } else {
+      // Else, hide the warning container
       warningContainer.classList.remove('view');
     }
   }, 400);
 });
 
+// Hide the modal when the dismiss button is clicked
 warningDismiss.addEventListener('click', e => {
   e.preventDefault();
-
   warningContainer.classList.remove('view');
 });
 
--- demos/react/src/components/AppForm.jsx
+++ demos/react/src/components/AppForm.jsx
@@ -5,46 +5,65 @@ import { run } from '@zootools/email-spell-checker';
 function AppForm() {
   const [email, setEmail] = useState('');
   const [suggestedEmail, setSuggestedEmail] = useState({});
+  // For closing the modal independent from any other checks,
+  // useful when you want to close it before running a debounced function that messes with the modal
+  // TLDR; if `forceCloseModal` is true, even if `suggestedEmail` is available, the modal won't be visible.
   const [forceCloseModal, setForceCloseModal] = useState(false);
 
+  // runs useBounce and sets `bounchedEmail` every time `email` changes
   let debouncedEmail = useDebounce(email, 400);
 
   useEffect(() => {
+    // Get suggested email
     const suggested = run({
       email: debouncedEmail,
     });
 
+    // If we have a result,
     if (suggested) {
+      // Set it to `suggestedEmail`
       setSuggestedEmail(suggested);
     } else {
+      // If not, force close the modal
       setForceCloseModal(true);
       setTimeout(() => {
+        // 500ms later (after animation finishes) clear suggested email
         setSuggestedEmail({});
       }, 500);
     }
   }, [debouncedEmail]);
 
+  // Sets `email` to `val`
   const handleEmailChange = val => {
     setEmail(val);
   };
 
+  // Sets `email` to the suggested one
   const handleFixEmail = () => {
+    // Force close modal
     setForceCloseModal(true);
 
+    // Set a timeout for updating email
     setTimeout(() => {
       setEmail(suggestedEmail.full);
     }, 500);
 
+    // Set a delayed timeout for enabling the modal again after the email gets set
+    // The reason we delay this is because the `debouncedEmail` variable gets set twice due to the previous timeout,
+    // and this forces it to become visible again, so we force close it until debouncedEmail is done updating
     setTimeout(() => {
       setForceCloseModal(false);
     }, 900);
   };
 
+  // Clears `suggestedEmail`
   const handleClearSuggestedEmail = e => {
     e.preventDefault();
 
+    // Force close modal,
     setForceCloseModal(true);
     setTimeout(() => {
+      // after animation finishes, clear `suggestedEmail` and remove force disable on modal
       setSuggestedEmail({});
       setForceCloseModal(false);
     }, 500);
